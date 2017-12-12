f438939a5a91a6300fc8c2159ab6fc790c1823ee
is this the google search bar? where is my grandson thomas? <8mjoyner@stpatrick.org>
2017-12-12 14:22:35 -0600

    holpe helpe help me pls

---
+++ BookFactory.js
@@ -0,0 +1,17 @@
+const ISBN = require("./ISBN.js");
+const EAN = require("./EAN.js");
+
+const BookFactory = function (){
+
+    function createBook(pubYear){
+      if(pubYear < 2007){
+        return new ISBN();
+      }
+      else{
+        return new EAN();
+      }
+    }
+    return {createBook};
+}
+
+module.exports = BookFactory;
